8cb6bcc4cee51fa9f5bc7ac6c777f80eae79f29a
GitHub Action <action@github.com>
2023-06-02 10:45:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/sillyfish/intellijpluginfirst/MyBundle.kt
+++ src/main/kotlin/com/github/sillyfish/intellijpluginfirst/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sillyfish.intellijpluginfirst
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sillyfish/intellijpluginfirst/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sillyfish/intellijpluginfirst/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.sillyfish.intellijpluginfirst.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sillyfish.intellijpluginfirst.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
